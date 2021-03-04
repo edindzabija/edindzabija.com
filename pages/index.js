1880be64ee211c8097853bfea4e5312d73834131
Edin Dzabija <edindzabija@gmail.com>
2021-03-04 20:05:13 +0100

    init

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Next App</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <main>
+      </main>
+    </div>
+  )
+}
+
+export default Home
